e29109002f7e050a7a581bc58e1b4bb19aba7273
Nasser Jamal <nasserjamal30@gmail.com>
2023-07-24 18:39:29 +0300

    start

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,17 @@
+import $ from "jquery";
+
+function addParagraphs() {
+    const paragraphs = [
+        'Holberton Dashboard',
+        'Dashboard data for the students',
+        'Copyright - Holberton School',
+    ];
+
+    paragraphs.forEach(element => {
+        $('body').append('<p>' + element + '</p>');
+    });
+}
+
+$(document).ready(function () {
+    addParagraphs();
+});
