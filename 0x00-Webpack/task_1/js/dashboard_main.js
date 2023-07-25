a5d6af308d0461602f22575658f901cccfc28496
Nasser Jamal <nasserjamal30@gmail.com>
2023-07-25 10:48:31 +0300

    attempt 3

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -6,7 +6,6 @@ $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append("<p id='count'></p>");
 $('body').append("<p>Copyright - Holberton School</p>");
-
 let count = 0;
 
 function updateCounter() {
