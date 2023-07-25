838fc6ad45fd9fa8d2c5afa27805a45fb41f7d2f
Nasser Jamal <nasserjamal30@gmail.com>
2023-07-25 10:53:28 +0300

    t

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -2,6 +2,8 @@ import $ from 'jquery';
 import _ from 'lodash';
 $(function() {
     let count = 0;
+
+    
     $('body').append('<p>Holberton Dashboard</p>');
     $('body').append('<p>Dashboard data for the students</p>');
     $('body').append('<button>Click here to get started</button>');
